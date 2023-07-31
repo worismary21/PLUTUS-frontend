558e683af42a81be3f4dfd638cdb271e2be98def
Elyzsium <fadiperukayat@gmail.com>
2023-07-31 23:49:27 +0100

    update signup page

---
+++ client/src/pages/signUp/signUp.tsx
@@ -0,0 +1,96 @@
+
+import { useState, FormEvent } from "react";
+import logo from '../../assets/logo.png'
+import "./signup.css";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+
+interface SignupComponent{
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+}
+
+
+
+const SignUp = () => {
+  const [formData, setFormData] = useState<SignupComponent>({
+    firstName:"",
+    lastName:"",
+    email: "",
+    password: "",
+  })
+
+  const [cPassword, setCPassword] = useState("");
+
+  const handleChange = (e:any) => {
+    const {name, value} = e.target
+    setFormData({
+      ...formData, 
+      [name]:value    
+    })
+
+   
+  }
+  console.log(formData)
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (formData.password !== cPassword) {
+      toast.error("Invalid Password");
+    } else {
+      toast.success("Registration Successful...");  
+    }
+  }
+ 
+
+ 
+  return (
+    <>
+         <section>
+         <div className="container">
+         <div className="left-side">
+              <div className="slide">
+              <img src={logo} alt="SignUp" width="350" />
+               <div className="left-text">
+                <h3>Plutus is personal <br /> finance, made simple.</h3>
+                <p>All your account, cards, savings, and investments in <br/> one place </p>
+               </div>
+              </div>
+         </div> 
+         <div className="right-side">
+          <div className="logo">
+            <h3 className="logo-one">Plutus</h3>
+            <p className="logo-two">Online Banking </p>
+          </div>
+         <div className="right-form">
+            <div className="heading">
+            <h2>Sign Up</h2>
+            <span>Sign up and enjoy the services</span>
+            </div>
+         <form action=""  onSubmit={handleSubmit}>
+           <input type="text" name={'firstName'} required value={formData.firstName} onChange={handleChange} placeholder=" FirstName" />
+           <input type="text" name={'lastName'} required value={formData.lastName} onChange={handleChange} placeholder=" LastName" />
+           <input type="email" name={'email'} required value={formData.email} onChange={handleChange} placeholder="Email" />
+           <input type="password" name={'password'} required value={formData.password}  onChange={handleChange} placeholder="Password" />
+           <input type="password" name={'cPassword'}required placeholder="Confirm Password"  value={cPassword} onChange={(e) => setCPassword(e.target.value)} />
+           <button type="submit" className=""> Register</button>
+          </form>
+      
+            <span className="register">
+              <p className="already">Already have an account?</p>
+              <Link to="/login">Login</Link>
+            </span>
+         </div>
+         </div>
+         </div>
+         </section>
+   </>
+  )
+}
+
+export default SignUp
+
