8eaca42a8838cc361006ad6e837ce9e9a104d30b
Samuel <36992355+Samuel-Adeyeye@users.noreply.github.com>
2023-08-09 02:13:51 +0100

    Updated dashboardhome

--- client/src/pages/Dashboard/DashboardHome.tsx
+++ client/src/pages/Dashboard/DashboardHome.tsx
@@ -1,58 +1,162 @@
-import orange from "../../assets/orange.png"
-import purple from "../../assets/purple.png"
+
 import graph from "../../assets/graph.png"
+import ProgressBar from './ProgressBar';
+import {
+  BsFillArrowUpCircleFill, 
+  BsFillCaretUpFill,
+  BsCaretDownFill,
+} from "react-icons/bs"
+import {FaGreaterThan, FaLessThan, FaPlane} from 'react-icons/fa'
 import "../../App.css"
 
-const DashboardHome = () => {
 
+
+
+const DashboardHome = () => {
+  
 
   return (
     <>
       <div className="flex flex-row w-full h-full">
-        <div className="flex flex-col">
-          <div className="  flex flex-row">
-            <div className="content w-full h-48 p-4 m-3 ">
-              <h3>Total balance</h3>
-              <h1 className="font-bold m-2 text-2xl leading-9 p-2">USD 10,000.00</h1>
+        <div className="flex flex-col p-10">
+          <div className="  flex flex-row mb-5">
+            <div className="content w-full h-48 p-4 mr-5">
+              <div className="flex flex-column justify-between">
+                <h3>Total balance</h3>
+                <div className="flex flex-column justify-end ">
+                  <div className="ml-11 mt-1 text-teal-500">
+                <BsFillArrowUpCircleFill />
+                  </div>
+                <p className="mr-10 text-teal-500" >2.36%</p>
+                </div>
+              </div>
+              <h1 className="font-bold m-2 text-3xl leading-9 p-2">USD 10,000.00</h1>
               <div className="flex flex-row">
                 <div className="">
-                  <p>income</p>
-                  <h4>USD 30,000.00</h4>
+                  <div>
+                    <div className="flex flex-column  ">
+                    <div className="ml-11 mt-1 text-teal-500">
+                        <BsFillCaretUpFill />
+                    </div>
+                    <p className="text-xs font-normal">Income</p>
+                    </div>
+                    <h4 className="font-bold">USD 30,000.00</h4>
+                  </div>
                 </div>
                 <div className="">
-                  <p>expenses</p>
-                  <h4>USD 30,000.00</h4>
+                  <BsCaretDownFill/>
+                  <p className="text-xs font-normal">Expenses</p>
+                  <h4 className="font-bold">USD 30,000.00</h4>
                 </div>
               </div>
             </div>
 
-            <div className="content w-full h-48 p-4 m-3 ">
-              <h3>Total savings</h3>
+            <div className="content w-full h-48 p-4 ">
+            <div className="flex flex-column justify-between">
+                <h3>Total savings</h3>
+                <div className="flex flex-column justify-end ">
+                  <div className="ml-11 mt-1 text-teal-500">
+                <BsFillArrowUpCircleFill />
+                  </div>
+                <p  className="mr-10 text-teal-500">2.36%</p>
+                </div>
+              </div>
               <h1 className="font-bold m-2 text-2xl leading-9 p-2">USD 5,000.00</h1>
               <div className="flex flex-row">
               </div>
             </div>
           </div>
-          <div className="h-80">
+          <div className="h-96 mb-2">
             <img src={graph} alt="graph" />
           </div>
           <div className="flex flex-row"> 
-            <div className="h-20 w-1/2">
-            <img src={purple} alt="graph" />
+            <div className="goals h-40 p-4 mr-5 w-1/2">
+            <div className="flex flex-column justify-between">
+               <h3 className="font-black ">Goals</h3>
+                <div className="flex flex-column pl-10 justify-end ">
+                  <div className="ml-11 mt-1 ">
+                  <FaLessThan style={{ width: '1rem', height: '1rem' }} />
+                  </div>
+                  <div className="mr-10 mt-1 ">   
+                    <FaGreaterThan style={{ width: '1rem', height: '1rem' }}/>
+                  </div>
+                </div>
+              </div>
+              <div>
+              <p>Summer Vacation</p>
+              </div>
+              <div>
+                <div>
+                  <div className="flex flex-column ">
+                  <div className="bg-white rounded-full flex items-center justify-center w-12 h-12">
+                  <FaPlane className=" w-6 h-6" />
+                 </div>
+                  <h2><b>62% reached</b></h2>
+                  </div>
+                  <div></div>
+                  <p>$1,485 out of $2,385</p>
+                </div>
+              </div>
             </div>
-            <div className="h-20 w-1/2"> 
-            <img src={orange} alt="graph" />
+            <div className="spendingOverview flex flex-column p-4 h-40 w-1/2"> 
+              <div className=" h-20">
+                <h4 className="font-black">Spending Overview</h4>
+                <div>
+               
+                <div className="w-3/2">
+                  <div className="flex flex-column justify-between  items-center">
+                      <ProgressBar percentage={68} />
+                    <p>
+                      <b className="ml-10">68%</b> 
+                      </p>
+                      <p>Groceries</p>
+                   </div>
+                  </div>
+                  <div className="w-/2">
+                  <div className="flex flex-column justify-between  items-center">
+                      <ProgressBar percentage={20} />
+                    <p>
+                      <b className="ml-10">20%</b> 
+                      </p>
+                      <p>Withdrawal</p>
+                   </div>
+                  </div>
+                  <div className="w-3/2">
+                  <div className="flex flex-column justify-between  items-center">
+                      <ProgressBar percentage={10} />
+                    <p>
+                      <b className="ml-10">10%</b> 
+                      </p>
+                      <p>Retail</p>
+                   </div>
+                  </div>
+                  <div className="w-3/2">
+                  <div className="flex flex-column justify-between  items-center">
+                      <ProgressBar percentage={2} />
+                    <p>
+                      <b className="ml-10">2%</b> 
+                      </p>
+                      <p>Leisure</p>
+                   </div>
+                 </div>                  
+                </div>
+            </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-column w-3/5">
-          <div>
-            Transactions
+        <div className=" w-3/5">
+          <div className="flex flex-row  p-5 pr-10 " >
+            <h4 className="text-xl font-medium leading-7">Transactions</h4>
+            <p className="text-end"> View all</p>
+          </div> 
+            <div className="flex flex-row w-44 pl-5">
+              <p className="mr-3">All</p>
+              <p className="mr-3">Expenses</p>
+              <p>Income</p>
           </div>
-          <div>
+          <div className="h-1/4">
             Beneficiary
           </div>
-
         </div>
         
       </div>
