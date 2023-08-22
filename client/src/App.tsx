d4ee7696ef16f9c0a2fbcfcae0c39929d60d4b77
Eluan96 <eluan.ea@gmail.com>
2023-08-23 00:58:15 +0100

    final commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,58 +1,56 @@
-import ChangePass from './pages/changePass/ChangePass'
-import ChangePass2 from './pages/changePass/ChangePass2'
-import Error404 from './pages/error404/Error404'
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import ChangePass from "./pages/changePass/ChangePass";
+import ChangePass2 from "./pages/changePass/ChangePass2";
+import Error404 from "./pages/error404/Error404";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Verify from "./pages/verify/Verify";
-import "./App.css"
-import SignUp from './pages/signUp/signUp';
-import Login from './pages/login/login'
-import { RoutesDashBoard } from './pages/Dashboard/Routes';
-import Loggin from './pages/onboarding/Loggin';
-import Signupp from './pages/onboarding/Signupp';
-import Homepage from './pages/homepage/Homepage';
-import Transactions from './pages/AdminPageTransactions/AllTransactions'
+import "./App.css";
+import SignUp from "./pages/signUp/signUp";
+import Login from "./pages/login/login";
+import { RoutesDashBoard } from "./pages/Dashboard/Routes";
+import Loggin from "./pages/onboarding/Loggin";
+import Signupp from "./pages/onboarding/Signupp";
+import Homepage from "./pages/homepage/Homepage";
+// import Transactions from "./pages/AdminPageTransactions/AllTransactions";
 // import Transactions from './pages/Transactions/Transactions';
-import DashboardHome from './pages/Dashboard/HomeDashBoard/DashboardHome';
+import DashboardHome from "./pages/Dashboard/HomeDashBoard/DashboardHome";
 
-import CompanyTable from './pages/companytable/CompanyTable';
+// import CompanyTable from "./pages/companytable/CompanyTable";
 // import React from 'react';
 
 function App() {
-
   return (
     <>
-     <BrowserRouter>
-          <ToastContainer
+      <BrowserRouter>
+        <ToastContainer
           theme="dark"
           position="top-right"
           autoClose={3000}
-          closeOnClick     
-          pauseOnHover={false}      
+          closeOnClick
+          pauseOnHover={false}
         />
-   
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-       <Route path='/signup' element={<SignUp />}/>
-       <Route path="/verify" element={<Verify />} />
-       <Route path="/changePassword" element={<ChangePass />}/>
-       <Route path="/login" element={<Login />} />
-       <Route path="/loggin" element={<Loggin />} />
-       
-       <Route path="/signupp" element={<Signupp />} />
-     <Route path='*' element={<Error404 />}/>
-     <Route path='/changePasswordConfirm' element={<ChangePass2 />}/>  
-     {/* <Route path='/dashboard/*' element={<Dashboard />} /> */}
-     {/* <Route path="/transactions" element={<Transactions/>}/> */}
-     <Route path='/dashboard/*' element={<RoutesDashBoard />} />
-     
-     <Route path='/dashboardhome' element={<DashboardHome />} />
 
-      </Routes>
-     </BrowserRouter> 
-     </>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/changePassword" element={<ChangePass />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/loggin" element={<Loggin />} />
 
-)}
+          <Route path="/signupp" element={<Signupp />} />
+          <Route path="*" element={<Error404 />} />
+          <Route path="/changePasswordConfirm" element={<ChangePass2 />} />
+          {/* <Route path='/dashboard/*' element={<Dashboard />} /> */}
+          {/* <Route path="/transactions" element={<Transactions/>}/> */}
+          <Route path="/dashboard/*" element={<RoutesDashBoard />} />
 
-export default App
+          <Route path="/dashboardhome" element={<DashboardHome />} />
+          {/* <Route path="dashboard/companies" element={<CompanyTable />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
+export default App;
--- client/src/pages/companytable/CompanyTable.tsx
+++ client/src/pages/companytable/CompanyTable.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useState } from 'react';
-import emptyProduct from './CompanyData';
-import { getCompanies } from '../../redux/action';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import CreateCompany from '../Dashboard/Company/CreateCompany.tsx'
+import React, { useEffect, useState } from "react";
+import emptyProduct from "./CompanyData";
+import { getCompanies } from "../../redux/action";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import CreateCompany from "../Dashboard/Company/CreateCompany.tsx";
 
 interface Company {
   id: number;
@@ -29,7 +29,7 @@ const CompanyTable: React.FC = () => {
   //   }catch(error){
   //     console.log(error)
   //   }
-  
+
   // }
 
   // console.log("company", comp)
@@ -37,25 +37,24 @@ const CompanyTable: React.FC = () => {
   // useEffect(() => {
   //   getCompanies()
   // }, [])
-const dispatch = useDispatch() as unknown as any
+  const dispatch = useDispatch() as unknown as any;
 
-const companies = useSelector((state:any) => state.company)
+  const companies = useSelector((state: any) => state.company);
 
-
-console.log(companies)
+  console.log(companies);
 
   useEffect(() => {
-    dispatch(getCompanies())
-  }, [])
-
-
+    dispatch(getCompanies());
+  }, []);
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
   const handleDelete = (id: number) => {
-    const updatedCompanyData = companyData?.filter(company => company.id !== id);
+    const updatedCompanyData = companyData?.filter(
+      (company) => company.id !== id
+    );
     setCompanyData(updatedCompanyData);
   };
 
@@ -63,32 +62,34 @@ console.log(companies)
   const endIndex = startIndex + companiesPerPage;
   const companiesToDisplay = companies?.slice(startIndex, endIndex);
 
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
 
   return (
-    <div className='p-4 md:p-8 lg:p-16'>
-      <h1 className='text-2xl md:text-3xl font-semibold text-center pb-4'>List of Companies</h1>
-      <div className='overflow-x-auto'>
-        <table className='w-full table-fixed'>
+    <div className="p-4 md:p-8 lg:p-16">
+      <h1 className="text-2xl md:text-3xl font-semibold text-center pb-4">
+        List of Companies
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="w-full table-fixed">
           <thead>
-            <tr className='bg-gray-200'>
-              <th className='w-1/6 py-2 px-4 text-left'>ID</th>
-              <th className='w-2/6 py-2 px-4 text-left'>Company Name</th>
-              <th className='w-1/6 py-2 px-4 text-left'>Investment</th>
-              <th className='w-1/6 py-2 px-4 text-left'>Business Type</th>
-              <th className='w-1/6 py-2 px-4 text-right'>Actions</th>
+            <tr className="bg-gray-200">
+              <th className="w-1/6 py-2 px-4 text-left">ID</th>
+              <th className="w-2/6 py-2 px-4 text-left">Company Name</th>
+              <th className="w-1/6 py-2 px-4 text-left">Investment</th>
+              <th className="w-1/6 py-2 px-4 text-left">Business Type</th>
+              <th className="w-1/6 py-2 px-4 text-right">Actions</th>
             </tr>
           </thead>
           <tbody>
-            {companiesToDisplay?.map((company:any) => (
-              <tr key={company.id} className='border-b hover:bg-gray-100'>
-                <td className='py-2 px-4'>{company.id}</td>
-                <td className='py-2 px-4'>{company.companyName}</td>
-                <td className='py-2 px-4'>{company.investment}</td>
-                <td className='py-2 px-4'>{company.businessType}</td>
-                <td className='py-2 px-4 text-right'>
+            {companiesToDisplay?.map((company: any) => (
+              <tr key={company.id} className="border-b hover:bg-gray-100">
+                <td className="py-2 px-4">{company.id}</td>
+                <td className="py-2 px-4">{company.companyName}</td>
+                <td className="py-2 px-4">{company.investment}</td>
+                <td className="py-2 px-4">{company.businessType}</td>
+                <td className="py-2 px-4 text-right">
                   <button
-                    className='bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded'
+                    className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded"
                     onClick={() => handleDelete(company.id)}
                   >
                     Delete
@@ -99,23 +100,31 @@ console.log(companies)
           </tbody>
         </table>
       </div>
-      <div className='flex justify-center mt-4'>
+      <div className="flex justify-center mt-4">
         <button
-          className='mr-2 hover:underline'
+          className="mr-2 hover:underline"
           disabled={currentPage === 1}
           onClick={() => handlePageChange(currentPage - 1)}
         >
           &laquo; Previous
         </button>
         <button
-          className='hover:underline'
-          disabled={currentPage === Math.ceil(companyData.length / companiesPerPage)}
+          className="hover:underline"
+          disabled={
+            currentPage === Math.ceil(companyData.length / companiesPerPage)
+          }
           onClick={() => handlePageChange(currentPage + 1)}
         >
           Next &raquo;
         </button>
       </div>
-      <button onClick={() => setModal(!modal)}> Add Company </button>
+      <button
+        onClick={() => setModal(!modal)}
+        className="bg-black w-[20%] text-white ml-[40%] h-[6vh] rounded-full hover:bg-blue-600 transform -translate-y-5 "
+      >
+        {" "}
+        Add Company{" "}
+      </button>
       {modal ? <CreateCompany /> : null}
     </div>
   );
